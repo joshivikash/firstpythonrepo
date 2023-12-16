6edbe44053a98c3646d3793ed74fc46dcb6e1089
Vikash Joshi <joshi.vikash17@gmail.com>
2023-12-16 19:26:00 +0530

    fixing binary search

--- week8/binary_search.py
+++ week8/binary_search.py
@@ -10,11 +10,12 @@ def binary_search(L, e):
     if L[i] == e:
         return True
     elif L[i] > e:
-        return binary_search(L[i+1:], e)
-    else:
         return binary_search(L[:i], e)
+    else:
+        return binary_search(L[i+1:], e)
     
 L = random.sample(range(100), 10)
+L.sort()
 print('List {}'.format(L))
-present = binary_search(L, 4)
+present = binary_search(L, 50)
 print(present)
